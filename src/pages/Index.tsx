10897d04e541e2fc4d7fda2a7d0ec4e89f538007
Anusheel Chapagain <chapagainanusheel@gmail.com>
2025-09-26 16:37:12 -0230

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -65,7 +65,7 @@ const Index = () => {
                 className="cursor-pointer"
               />
             </a>
-            <p className="text-muted-foreground text-lg -mt-9">
+            <p className="text-muted-foreground text-lg -mt-16">
               Do you even prompt bro !
             </p>
           </div>
