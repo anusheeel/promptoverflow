e574184682b4edd0308ff0de2a5b5005e5984771
Anusheel Chapagain <chapagainanusheel@gmail.com>
2025-09-26 16:27:19 -0230

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -60,7 +60,7 @@ const Index = () => {
               <img
                 src="/pof_logo.png"
                 alt="Prompt Overflow Logo"
-                width={140}   // made bigger
+                width={240}   // made bigger
                 height={140}
                 className="cursor-pointer"
               />
